e3a2926eb4a168a488e6e99c156c9de14c95e604
ron <lucifron28@gmail.com>
2025-05-31 21:21:05 +0800

    alignments

--- todo/script.js
+++ todo/script.js
@@ -1,9 +1,19 @@
 let allTasks = ["run", "buy groceries", "clean the house", "finish homework"];
 
 let taskList = document.getElementById("tasks");
+let addTastButton = document.getElementById("add-task-button");
+let clearTasksButotn = document.getElementById("clear-tasks-button");
 
 allTasks.forEach(task => {
     let li = document.createElement("li");
-    li.textContent = task;
+    let checkbox = document.createElement("input");
+    let textContent = document.createTextNode(task);
+    let spanText = document.createElement("span");
+    spanText.appendChild(textContent);
+    li.appendChild(spanText);
+    checkbox.type = "checkbox";
+    checkbox.className = "task-checkbox";
+    li.appendChild(checkbox);
+    li.className = "task-item";
     taskList.appendChild(li);
 })
