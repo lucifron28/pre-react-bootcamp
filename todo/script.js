5007d3deba0bfcd5e9b1cf0b08e345f34c2cfe4b
ron <lucifron28@gmail.com>
2025-05-31 23:09:16 +0800

    modall added

--- todo/script.js
+++ todo/script.js
@@ -1,19 +1,33 @@
-let allTasks = ["run", "buy groceries", "clean the house", "finish homework"];
+document.addEventListener("DOMContentLoaded", () => {
 
-let taskList = document.getElementById("tasks");
-let addTastButton = document.getElementById("add-task-button");
-let clearTasksButotn = document.getElementById("clear-tasks-button");
+    let allTasks = ["run", "buy groceries", "clean the house", "finish homework"];
 
-allTasks.forEach(task => {
-    let li = document.createElement("li");
-    let checkbox = document.createElement("input");
-    let textContent = document.createTextNode(task);
-    let spanText = document.createElement("span");
-    spanText.appendChild(textContent);
-    li.appendChild(spanText);
-    checkbox.type = "checkbox";
-    checkbox.className = "task-checkbox";
-    li.appendChild(checkbox);
-    li.className = "task-item";
-    taskList.appendChild(li);
-})
+    let taskList = document.getElementById("tasks");
+    let addTaskButton = document.getElementById("add-task-button");
+    let clearTasksButton = document.getElementById("clear-tasks-button");
+    let cancelButton = document.getElementById("cancel-button");
+    let modal = document.getElementById("modal");
+
+    addTaskButton.addEventListener("click", () => {
+        modal.className = "active";
+    });
+
+    cancelButton.addEventListener("click", () => {
+        modal.className = "";
+    });
+
+    allTasks.forEach(task => {
+        let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+        let textContent = document.createTextNode(task);
+        let spanText = document.createElement("span");
+        spanText.appendChild(textContent);
+        li.appendChild(spanText);
+        checkbox.type = "checkbox";
+        checkbox.className = "task-checkbox";
+        li.appendChild(checkbox);
+        li.className = "task-item";
+        taskList.appendChild(li);
+});
+
+});
