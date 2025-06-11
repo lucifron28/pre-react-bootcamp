1928c9602eb923bfc74a5c99c9bc6a72dd795337
ron <lucifron28@gmail.com>
2025-06-11 11:48:21 +0800

    initialize all elements

---
+++ counter/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counterElement = document.getElementById("counter");
+    const incrementButton = document.getElementById("increment-button");
+    const decrementButton = document.getElementById("decrement-button");
+    let counter = 0; 
+});
